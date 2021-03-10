4648481ae46c1570fecf9f89283d9d9f69b8bb10
Colton Medlin <coltonmedlin@gmail.com>
2021-03-09 16:15:39 -0800

    Complete Adding Post Routes

--- server/app.js
+++ server/app.js
@@ -17,66 +17,121 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+
+app.get('/login',
+  (req, res) => {
+    res.render('login');
+  });
+
+app.post('/login',
+  (req, res, next) => {
+    //request to database for username
+    models.Users.get({username: req.body.username})
+    //check the attempted password against the password in the database user using user .compare
+      .then((user) => {
+        if (user === undefined) {
+          throw new Error('user does not exist');
+        }
+        return models.Users.compare(req.body.password, user.password, user.salt);
+      }).then((success) => {
+        //if they match, return 200 with redirect
+        if (success) {
+          res.status(200);
+          res.redirect('/');
+        } else {
+          throw new Error('wrong password');
+        }
+      }).catch((err) => {
+        res.status(401);
+        res.redirect('/login');
+        res.end();
+      });
+  });
+
+app.get('/signup',
+  (req, res) => {
+    res.render('signup');
+  });
+
+app.post('/signup',
+  (req, res, next) => {
+    models.Users.get({ username: req.body.username })
+      .then((user) => {
+        if (user === undefined) {
+          models.Users.create({ username: req.body.username, password: req.body.password })
+            .then((err, success) => {
+              res.status(200);
+              res.redirect('/');
+            }).catch((err) => {
+              res.status(400);
+              res.end();
+            });
+        } else {
+          res.status(401);
+          res.redirect('/signup');
+        }
+      });
+  });
 
 
 
