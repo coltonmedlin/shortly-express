552be894bc4cdb730111bb8c56013973ad85c262
Colton Medlin <coltonmedlin@gmail.com>
2021-03-09 20:49:56 -0800

    eod 3/9

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,37 @@
 const parseCookies = (req, res, next) => {
+  let cookieObj = {};
+  //THIS MAY BE WRONG VVV
+  let cookieString = req.headers.cookie;
+  console.log('string', cookieString);
+  //take the cookie text
+  let key = '';
+  let value = '';
+  let isKey = true;
+  for (let i = 0; i < cookieString.length; i++) {
+    if (cookieString[i] === '=') {
+      isKey = false;
+      i++;
+    }
+    if (isKey) {
+      key += cookieString[i];
+    }
+    if (cookieString[i] === ';' || i === cookieString.length - 1) {
+      isKey = true;
+      cookieObj[key] = value;
+      key = '';
+      value = '';
+      i + 2;
+    }
+    if (!isKey) {
+      value += cookieString[i];
+    }
+  }
+  console.log(cookieObj);
+  //loop though and build a key until we hit an =
+  //then build the value until we hit ;
+  //repeat until finished
+  //req.body.cookie = cookieObj;
+  next();
 };
 
 module.exports = parseCookies;
