25048cbe400b6ece65e14a19710a8821c7061794
Colton Medlin <coltonmedlin@gmail.com>
2021-03-11 14:43:22 -0800

    added comments to createSession

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,23 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
-module.exports.createSession = (req, res, next) => {
-  // let sessionObject = {};
-  // //grab the cookie object from the body
-  // let hash = req.cookie.shorltyid;
-  // //look up user data related to that session
-  // models.Sessions.getAll({hash})
-  //   .then((userSession)=>{
-  //     if (userSession === undefined) {
+/*
+ * access the parsed cookie
+ * lookup user data
+ * assign an object to the SESSION property with relivant info (should have hash)
+ *
+ * ------------------
+ *
+ * A request with no cookies should should generate a session and
+ * store it in the database it should also set a cookie in the response header
+ * cookie set with 'shortlyid' set to hash
+ *
+ * If there is a cookie it should be verified
+ *
+*/
 
-  //     }
-  //   });
+module.exports.createSession = (req, res, next) => {
+
   models.Sessions.create()
     .then((response) => {
       console.log('response: ', response);
@@ -28,9 +34,6 @@ module.exports.createSession = (req, res, next) => {
     .catch(() => {
       next();
     });
-
-  //assign to session property on the request
-  //req.session = sessionObject;
 
 };
 
